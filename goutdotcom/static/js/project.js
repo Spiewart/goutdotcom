83b5d51b622ffd2650d5ad85855d69dc7d455ad0
Spiewart <spiewart@gmail.com>
2021-04-16 16:14:31 -0500

    Working on JS for ULT form

--- goutdotcom/static/js/project.js
+++ goutdotcom/static/js/project.js
@@ -1,23 +1,30 @@
 /* Project specific Javascript goes here. */
 
 // function that hides/shows field_four based upon first_flare value
-function check_first_flare(new_val) {
-    if(new_val == false) {
-        // #id_field_four should be actually the id of the HTML element
-        // that surrounds everything you want to hide.  Since you did
-        // not post your HTML I can't finish this part for you.
-        $('#div_id_num_flares').addClass('hidden');
-    } else {
-        $('#div_id_num_flares').removeClass('hidden');
-    }   
-  }
-    // this is executed once when the page loads
-  $(document).ready(function() {
-    // set things up so my function will be called when first_flare
-    $('#id_first_flare').change( function() {
-      check_first_flare(this.value);
-      alert("Hi");
-    });
-  });
+function check_first_flare() {
+  if ($(this).val() == true) {
+      // #id_field_four should be actually the id of the HTML element
+      // that surrounds everything you want to hide.  Since you did
+      // not post your HTML I can't finish this part for you.
+      alert("Hello\nHow are you?");
+      $('#div_id_num_flares').hide();
+      $('#div_id_freq_flares').hide();
+      $('#div_id_erosions').hide();
+      $('#div_id_tophi').hide();
+      $('#div_id_stones').hide();
+      $('#div_id_ckd').hide();
+      $('#div_id_uric_acid').hide();
+  } else {
+      $('#div_id_num_flares').show();
+      $('#div_id_freq_flares').show();
+      $('#div_id_erosions').show();
+      $('#div_id_tophi').show();
+      $('#div_id_stones').show();
+      $('#div_id_ckd').show();
+      $('#div_id_uric_acid').show();
+      alert($(this).val())
+      alert("Fruit");
+  }   
+}
 
 
