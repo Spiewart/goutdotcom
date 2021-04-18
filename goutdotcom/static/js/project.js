234810ddc14b4476d8029d1fe45b71eebfa9293e
Spiewart <spiewart@gmail.com>
2021-04-18 07:34:32 -0500

    Made the ULT submission form responsive to the first form field with javascript!

--- goutdotcom/static/js/project.js
+++ goutdotcom/static/js/project.js
@@ -1,12 +1,9 @@
 /* Project specific Javascript goes here. */
 
-// function that hides/shows field_four based upon first_flare value
+// function that hides/shows subsequent fields based upon first_flare value
 function check_first_flare() {
-  if ($(this).val() == true) {
-      // #id_field_four should be actually the id of the HTML element
-      // that surrounds everything you want to hide.  Since you did
-      // not post your HTML I can't finish this part for you.
-      alert("Hello\nHow are you?");
+  alert("Function Start");
+  if ($('#id_first_flare').val() == 'True') {
       $('#div_id_num_flares').hide();
       $('#div_id_freq_flares').hide();
       $('#div_id_erosions').hide();
@@ -22,8 +19,6 @@ function check_first_flare() {
       $('#div_id_stones').show();
       $('#div_id_ckd').show();
       $('#div_id_uric_acid').show();
-      alert($(this).val())
-      alert("Fruit");
   }   
 }
 
