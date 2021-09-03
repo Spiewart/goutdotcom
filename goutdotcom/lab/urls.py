c89eb0cc83ab87577d0bae37ec596e594ab70010
Spiewart <spiewart@gmail.com>
2021-09-03 18:28:37 -0500

    FlareAbout filling out.

--- goutdotcom/lab/urls.py
+++ goutdotcom/lab/urls.py
@@ -13,8 +13,8 @@ app_name = "lab"
 urlpatterns = [
     path("", view=IndexView.as_view(), name="index"),
     path("about/<lab>/", view=LabAbout.as_view(), name="about"),
-    path("create/<lab>/", view=LabCreate.as_view(), name="lab-create"),
-    path("list/<lab>/", view=LabList.as_view(), name="lab-list"),
-    path("update/<lab>/<int:pk>/", view=LabUpdate.as_view(), name="lab-update"),
-    path("<lab>/<int:pk>/", view=LabDetail.as_view(), name="lab-detail"),
+    path("create/<lab>/", view=LabCreate.as_view(), name="create"),
+    path("list/<lab>/", view=LabList.as_view(), name="list"),
+    path("update/<lab>/<int:pk>/", view=LabUpdate.as_view(), name="update"),
+    path("<lab>/<int:pk>/", view=LabDetail.as_view(), name="detail"),
 ]
