13e95a0b7d801aae8a432b88da9e0f8099fca162
Spiewart <spiewart@gmail.com>
2021-10-06 15:22:29 -0500

    Embarking on ULT model redesign with related models.

--- goutdotcom/ult/forms.py
+++ goutdotcom/ult/forms.py
@@ -9,9 +9,15 @@ from .models import ULT
 class ULTForm(forms.ModelForm):
     erosions = forms.BooleanField(required=False, help_text="Do you have erosions on x-rays or other imaging?")
     tophi = forms.BooleanField(required=False, help_text="Do you have gouty tophi?")
-    stones = forms.BooleanField(required=False, help_text="Do you get uric acid kidney stones?")
-    ckd = forms.BooleanField(required=False, help_text="Do you have chronic kidney disease (CKD)?")
-    uric_acid = forms.BooleanField(required=False, help_text="Is your uric acid over 9.0?")
+    stones = forms.BooleanField(
+        required=False, label="Uric acid kidney stones", help_text="Do you get uric acid kidney stones?"
+    )
+    ckd = forms.BooleanField(
+        required=False, label="Chronic kidney disease (CKD)", help_text="Do you have chronic kidney disease (CKD)?"
+    )
+    uric_acid = forms.BooleanField(
+        required=False, label="Hyperuricemia (elevated blood uric acid)", help_text="Is your uric acid over 9.0?"
+    )
 
     class Meta:
         model = ULT
