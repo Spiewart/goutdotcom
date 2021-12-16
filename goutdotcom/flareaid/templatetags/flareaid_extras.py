9ff30b10698db53261df18a37f0b2aa12637be3e
Spiewart <spiewart@gmail.com>
2021-12-15 18:33:47 -0600

    Working on FlareAid Detail. FlareAid decision_aid() method needs work, colchicine not working.

--- goutdotcom/flareaid/templatetags/flareaid_extras.py
+++ goutdotcom/flareaid/templatetags/flareaid_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def index(indexable, i):
     return indexable[i]
+
+@register.filter(name='get_key')
+def get_key(dictionary, key):
+    return dictionary.get(key)
